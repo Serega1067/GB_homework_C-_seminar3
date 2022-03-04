4cba72f76f02eeb520fe1f0e14750ab93bb21960
Serega S <serega1067@yandex.ru>
2022-03-04 18:03:06 +0700

    Task1 done

---
+++ homework_C#_seminar3_task1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и 
+проверяет, является ли оно палиндромом.
+*/
+
+string testNumber(int arg)
+{
+    bool flag = true;
+    int count = 1;
+    string numString = Convert.ToString(arg);
+    int numLen = numString.Length;
+    int numHalf = numLen / 2;
+    while (count < numHalf + 1)
+    {
+        if (numString[count - 1] != numString[numLen - count])
+        {
+            flag = false;
+        }
+        count++;
+    }
+    if (flag == true) return "Да";
+    else return "Нет";
+}
+
+Console.WriteLine("Введите число, чтобы проверить является ли оно " +
+                  "палиндромом: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(testNumber(num));
