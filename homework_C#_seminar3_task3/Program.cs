635ddc81431d6d846a212e159ae548f7eebd0d70
Serega S <serega1067@yandex.ru>
2022-03-04 22:40:17 +0700

    Task3 done

---
+++ homework_C#_seminar3_task3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт 
+таблицу кубов чисел от 1 до N.
+*/
+
+void DegreeNumber(double arg)
+{
+    double count = 1;
+    while (count <= arg)
+    {
+        Console.WriteLine(Math.Pow(count, 3));
+        count++;
+    }
+}
+
+Console.WriteLine("Введите число: ");
+double num = Convert.ToInt32(Console.ReadLine());
+DegreeNumber(num);
